60892db420df7f351c0871932e003638457fd791
uiys <uiysuiysuiysuiys@Gmail.com>
2023-10-25 10:48:37 +0800

    加上执行注释

--- apt-last-time/src/main/java/com/uiys/spi/AutoStarterServer.java
+++ apt-last-time/src/main/java/com/uiys/spi/AutoStarterServer.java
@@ -21,15 +21,27 @@ import javax.tools.Diagnostic;
 @AutoService(Processor.class)
 public class AutoStarterServer extends AbstractProcessor {
 
+	/**
+	 *
+	 * @param annotations the annotation types requested to be processed (需要处理的那些注解在init的时候就放进去了)
+	 * @param roundEnv  environment for information about the current and prior round (注解处理器的环境支持)
+	 * @return
+	 */
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+		// 将需要处理的注解循环
 		for (TypeElement an : annotations) {
+			// 得到需要处理的注解的所有元素(因为这个元素的是所有的 表示程序元素，如包、类或方法。每个元素表示静态的语言级构造 (而不是例如虚拟机的运行时构造))
 			Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(an);
+			// 过滤里面的类元素 (找到需要处理的类)
 			Set<TypeElement> types = ElementFilter.typesIn(elementsAnnotatedWith);
+			// 循环处理某一个类
 			for (TypeElement typeElement : types) {
+				// 根据注解的名称获取到 处理这个注解的处理器.
 				CodeGenProcessor codeGenProcessor = CodeGenAndAnnoRegister.getCodeGenProcessor(an.getQualifiedName()
 				  .toString());
 				try {
+					// 处理器利用上下文的环境 处理这个类元素
 					codeGenProcessor.generate(typeElement, roundEnv);
 				} catch (Exception e) {
 					ProcessingHolder.getProcessingEnv()
