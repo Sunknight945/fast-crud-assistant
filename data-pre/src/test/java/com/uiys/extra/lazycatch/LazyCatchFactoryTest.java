f7f407a08ebf084257e082d0e3147daf0e22e9d7
uiys <uiysuiysuiysuiys@Gmail.com>
2023-12-17 02:32:49 +0800

    调整

--- data-pre/src/test/java/com/uiys/extra/lazycatch/LazyCatchFactoryTest.java
+++ data-pre/src/test/java/com/uiys/extra/lazycatch/LazyCatchFactoryTest.java
@@ -5,36 +5,33 @@ import com.uiys.extra.lazycatch.support.LazyFieldLoaderFactory;
 import com.uiys.extra.lazycatch.support.LazyTypeInterceptorFactory;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * @author uiys
  */
 @SpringBootTest(classes = LazyCatchFactoryTest.class)
-@ComponentScan(basePackages = "com.uiys.extra.lazycatch")
-class LazyCatchFactoryTest implements ApplicationContextAware {
-
-	private ApplicationContext applicationContext;
-
+class LazyCatchFactoryTest {
 	@Test
 	public void testLazyCatch() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.register(LinkOneService.class);
+		applicationContext.register(LinkTwoService.class);
+		applicationContext.refresh();
 		LazyCatchFactory lazyCatchFactory =
 		  new LazyCatchFactoryDefault(new LazyTypeInterceptorFactory(new LazyFieldLoaderFactory(applicationContext)),
-		    applicationContext);
+			applicationContext);
+
 		ObjectLazy objectLazy = new ObjectLazy();
 		objectLazy.setLinkId("linkId");
+
 		ObjectLazy objectLazy1 = lazyCatchFactory.lazyCatch(objectLazy);
+
 		List<LinkObjOne> linkObjOnes = objectLazy1.getLinkObjOnes();
+
 		System.out.println("linkObjOnes = " + linkObjOnes);
 	}
 
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
 }
